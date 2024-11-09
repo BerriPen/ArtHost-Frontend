53c0b6ce626d6de9889ba8959c33e012c5c822ac
BerriPen <chanyeolkookielis@gmail.com>
2024-11-08 21:09:09 -0800

    added EventCard

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,13 +1,114 @@
 <template>
-     <q-card class="my-card">
-      <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-        <div class="absolute-bottom text-subtitle2 text-center">
-          Title
+  <q-page>
+    <div class="q-pa-xl q-ma-lg q-gutter-lg row ">
+      <q-card class="challenge-item" v-for="(event, index) in eventList" :key="index">
+        <q-img src="../assets/banners/1.jpg" fit="cover" class="full-width full-height"></q-img>
+        <div class="challenge-overlay">
+          <div class="content">
+            <div class="challenge-status" style="font-size: 16px;">
+              Challenge status
+            </div>
+            <a class="challenge-title" style="font-size: 44px;">
+              {{ event.name}}
+            </a>
+            <p class="challenge theme" style="font-size: 24px;">
+              {{ event.theme }}
+            </p>
+            <div class="challenge-info" style="font-size: 14px;">
+              <div class="row host-and-judges">
+                <div class="col challenge-count-challengers">
+                  Challengers count
+                </div>
+                <div class="col items-end host-users">
+                  <p>
+                    Hosts {{ event.host }}
+                  </p>
+                </div>
+                <div class="col judge-users">
+                  <p>
+                    Judges {{  event.judges }}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </q-img>
     </q-card>
+    </div>
+  </q-page>
 </template>
-
 <script>
+import axios from "axios";
 
+export default {
+  name: "IndexPage",
+  data() {
+    return {
+      eventList: null,
+    };
+  },
+  mounted() {
+    this.fetchEvents();
+  },
+  methods: {
+    async fetchEvents() {
+      console.log("fetching event details");
+      const url = "http://127.0.0.1:8000/api/main/eventList";
+
+      axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data);
+          this.eventList = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
+
+<style scoped>
+.challenge-item{
+  height: 200px;
+  border-radius: 0;
+  position: relative;
+  overflow: hidden;
+  width: 100%; 
+  height: 350px;
+}
+
+.challenge-banner {
+  height: 350px;
+  width: auto;
+  border-radius: 0;
+  overflow: hidden;
+}
+
+/*  DATII NA OVERLAY
+.challenge-overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  padding: 16px;
+  z-index: 2;
+  color: white;
+  background: linear-gradient(to top, rgba(3, 3, 3, 0.7), rgba(0, 0, 0, 0) 100%);
+
+} */
+
+.challenge-overlay {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  color: white;
+  padding: 16px;
+  z-index: 2;
+  background: linear-gradient(to top, rgba(3, 3, 3, 0.7), rgba(0, 0, 0, 0) 100%);
+}
+</style>
--- src/pages/EventPage.vue
+++ src/pages/EventPage.vue
@@ -1,33 +1,96 @@
 <template>
-  <q-page class="padding">
+  <q-page>
     <div>
-      HOME PAGE
+        <EventCard/>
+        HOME PAGE
     </div>
-    <div class="q-pa-md row">
-      <q-card style="width: 700px; height: 200px;" class="rectangle-card">
-        <q-img src="https://cdn.quasar.dev/img/parallax2.jpg" fit="cover" class="full-width full-height">
-          <!-- Overlay with fading effect -->
-          <div class="absolute bottom-0 left-0 right-0 gradient-blur-overlay">
-            <div>Status</div>
-            <div>Competition Name</div>
-            <div>Competition Subtitle</div>
-            <div class="row">              <div class="col">Competitors</div>
-              <div class="col justify-end">Host/Judges</div>
+    
+
+
+<!-- EXTRA CARRD  
+    <div class="challenge-item q-pa-xl">
+      <div class="challenge-banner">
+        <q-card>
+          <q-img src="https://cdn.quasar.dev/img/parallax2.jpg"></q-img>
+          <div class="challenge-overlay">
+            <div class="content">
+              <div class="challenge-status">
+                Challenge status
+              </div>
+              <a class="challenge-title">
+                Challenge title
+              </a>
+              <p class="challenge theme">
+                Challenge theme
+              </p>
+              <div class="challenge-info">
+                <div class="challenge-count">
+                  <div challenge-count-challengers>
+                    Challengers count
+                  </div>
+                </div>
+                <div class="row host-and-judges">
+                  <div class="col host-users">
+                    host
+                  </div>
+                  <div class="col host-users">
+                    judges
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
+        </q-card>  
+      </div>      
+    </div>
+
+
+    <div class="q-pa-xl q-ma-xl row">
+      <q-card class="challenge-item" v-for="(event, index) in eventList" :key="index">
+        <q-img src="../assets/banners/1.jpg" fit="cover" class="full-width full-height">
         </q-img>
+        <div class="challenge-overlay">
+          <div class="content">
+              <div class="challenge-status" style="font-size: 16px;">
+                Challenge status
+              </div>
+              <a class="challenge-title" style="font-size: 44px;">
+                {{ event.name}}
+              </a>
+              <p class="challenge theme" style="font-size: 24px;">
+                {{ event.theme }}
+              </p>
+              <div class="challenge-info" style="font-size: 14px;">
+                <div class="row host-and-judges">
+                  <div class="col challenge-count-challengers">
+                    Challengers count
+                  </div>
+                  <div class="col items-end host-users">
+                    <p>
+                      Host {{ event.host }}
+                    </p>
+                  </div>
+                  <div class="col judge-users">
+                    {{ event.judges }}
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
       </q-card>
 
       <div class="text-black" v-for="(event, index) in eventList" :key="index">
         {{ event }}
       </div>
     </div>
+  -->
+
   </q-page>
 </template>
 
 <script>
 import axios from "axios";
-
+import EventCard from "src/components/EventCard.vue";
 export default {
   name: "IndexPage",
   data() {
@@ -37,6 +100,9 @@ export default {
   },
   mounted() {
     this.fetchEvents();
+  },
+  components: {
+    EventCard
   },
   methods: {
     async fetchEvents() {
@@ -56,23 +122,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.rectangle-card {
-  height: 200px;
-  border-radius: 0px;
-  position: relative;
-  overflow: hidden;
-}
-.gradient-blur-overlay {
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  color: white;
-  padding: 16px;
-  background: linear-gradient(to top, rgba(3, 3, 3, 0.7), rgba(0, 0, 0, 0) 100%);
-  backdrop-filter: blur(5px);
-}
-</style>
-  
